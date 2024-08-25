e5648ef1631213d0e8e71053041ae4c4702e63d4
Ciara <125183922+ItsKiera@users.noreply.github.com>
2024-08-25 08:33:39 +0100

    Update InsuranceFormula.py

--- InsuranceFormula.py
+++ InsuranceFormula.py
@@ -1,59 +1,26 @@
-# create the initial variables below
-age = 28
-sex = 0
-bmi = 26.2
-num_of_children = 3
-smoker = 0
+# Create calculate_insurance_cost() function below: 
+def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
+  estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+  print('The estimated insurance cost for ' + name + ' is ' + str(estimated_cost) + ' dollars.')
+  return estimated_cost
+  
+# Create difference_between_insurance() function below to calculate the difference between insurance costs of any two individuals: 
+def difference_between_insurance(first_person, second_person):
+  if first_person > second_person:
+    difference = first_person - second_person
+  else:
+    difference = second_person - first_person
+  print('The difference in insurance cost is ' + str(difference) + ' dollars.')
+  return difference
 
-# Add insurance estimate formula below
-insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-print("This person's insurance cost is " + str(insurance_cost) + " dollars.") 
+# Estimate Maria's insurance cost
+maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, name ='Maria')
 
-# Age Factor
-age += 4
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+# Estimate Omar's insurance cost 
+omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1, name ='Omar')
 
-change_in_insurance_cost = new_insurance_cost - insurance_cost
+# Estimate your own insurance cost
+ciara_insurance_cost = calculate_insurance_cost(24, 0, 24.2, 0, 0, name = 'Ciara')
 
-print('The change in cost of insurance after increasing the age by 4 years is ' + str(change_in_insurance_cost) + ' dollars.')
-
-age = 28
-
-# BMI Factor
-bmi += 3.1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-print('The change in estimated insurance cost after increasing BMI by 3.1 is ' + str(change_in_insurance_cost) + ' dollars.')
-
-bmi = 26.2
-# Male vs. Female Factor
-sex = 1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print('The change in estimated cost for being male instead of female is ' + str(change_in_insurance_cost) + ' dollars.')
-
-sex = 0
-# Extra Practice
-# non smoker and smoker
-smoker = 1
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print('The change in estimated cost for being a smoker instead of a non smoker is ' + str(change_in_insurance_cost) + ' dollars.')
-
-smoker = 0
-# having 3 children and having 6 children 
-num_of_children = 6
-
-new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print('The change in cost of insurance from having 6 children instead of 3 is ' + str(change_in_insurance_cost) + ' dollars.')
+# Estimates the difference between insurance costs
+difference_insurance = difference_between_insurance(maria_insurance_cost, omar_insurance_cost)
